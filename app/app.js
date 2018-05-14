b841a14c0d9f9208e215d55e30e159db44b10c79
Milksnatcher <ruben.aegerter@gmx.de>
2018-05-14 08:58:37 +0200

    add tests for jovo

--- app/app.js
+++ app/app.js
@@ -9,195 +9,101 @@ const {App} = require('jovo-framework');
 const config = {
     logging: true,
     intentMap: {
-        "AMAZON.PauseIntent": "CancelWorkoutIntent",
-        "AMAZON.NextIntent": "NextExerciseIntent",
-        'AMAZON.ResumeIntent': 'ResumeIntent',
+        "AMAZON.PauseIntent": "AudioPauseIntent",
+        "AMAZON.NextIntent": "AudioNextIntent",
+        'AMAZON.ResumeIntent': 'AudioResumeIntent',
         "AMAZON.YesIntent": "YesIntent",
         "AMAZON.NoIntent": "NoIntent",
-        "AMAZON.CancelIntent": "CancelWorkoutIntent",
-        "AMAZON.StopIntent": "CancelWorkoutIntent"
+        "AMAZON.CancelIntent": "AudioCancelIntent",
+        "AMAZON.StopIntent": "AudioStopIntent"
     },
+    db: {
+        type: 'file',
+        localDbFilename: 'test_db'
+    }
+
 };
 
 const app = new App(config);
-
-const countdownSound = "https://www.jovo.tech/audio/6ccwIA0o-sampleaudio-04mb.mp3";
-const workoutSound = "https://www.jovo.tech/audio/6ccwIA0o-sampleaudio-04mb.mp3";
-const restSound = "https://www.jovo.tech/audio/6ccwIA0o-sampleaudio-04mb.mp3";
-
-const workouts = {
-    Venus: {
-        title: "Venus",
-        exercises: [{exercise: "50 Pushups"}, {exercise: "20 Situps"}, {exercise: "50 Squads"}],
-        rounds: 4
-    },
-
-    Ares: {
-        title: "Ares",
-        exercises: [{exercise: "7 Pullups"}, {exercise: "7 Situps"}, {exercise: "40 meter sprint twice"}, {rest: "60 seconds"}],
-        rounds: 5
-    }
-};
 
 // =================================================================================
 // App Logic
 // =================================================================================
 
 app.setHandler({
-    'LAUNCH': function() {
+    'NEW_SESSION': function () {
+        this.user().data.new = true;
+    },
+
+    'LAUNCH': function () {
         let speech = this.speechBuilder()
             .addText(['Hey?', 'Hello?', 'Good Morning?']);
         let reprompt = 'Hello World?';
-        console.log('LAUNCH');
         this.ask(speech, reprompt);
     },
 
+    'END': function() {
+        this.tell('Session ended.');
+    },
+
+    'HelpIntent': function () {
+        this.tell('I\'m happy to help you.');
+    },
+
+    'TestForSlotIntent': function (name) {
+        this.tell(`Hey ${name.key}!`);
+    },
+
+    'TestSessionAttributesIntent': function () {
+        this.toStateIntent('TestSessionAttributesState', 'TestSessionAttributesIntent');
+    },
+
+    'TestSessionAttributesState': {
+        'TestSessionAttributesIntent': function () {
+            this.tell('I\'m in a state.');
+        },
+        'Unhandled': function() {
+            this.tell('Unhandled in a state.');
+        }
+    },
+
+    'AudioStopIntent': function() {
+        this.tell('Audio stopped.');
+    },
+
     'ON_EVENT': {
-        'AlexaSkillEvent.SkillDisabled': function() {
+        'AlexaSkillEvent.SkillDisabled': function () {
             this.endSession();
         },
     },
 
     'ON_ELEMENT_SELECTED': {
-        "token": function() {
+        "token": function () {
             this.endSession();
         }
     },
 
-    'TestIntent': function () {
-        this.toStateIntent('TestState', 'Hello');
-    },
-
-    'TestState': {
-        'Hello': function() {
-            this.followUpState('TestState').ask('Hello World?');
-        },
-        'YesIntent': function() {
-            this.tell('YES');
-        }
-    },
-
-    "EnterWorkoutIntent": function(workout) {
-        this.toIntent("StartWorkoutIntent", workout);
-    },
-
-    "StartWorkoutIntent": function(workout) {
-        this.user().data.rounds = 0;
-        this.user().data.exercise = 0;
-        if(!workouts[workout.key]) {
-            this.tell('This workout doesn\'t exist.');
-            return;
-        }
-        this.user().data.workout = workouts[workout.key];
-        this.alexaSkill().audioPlayer().setOffsetInMilliseconds(0)
-            .play(workoutSound, "token")
-            .tell(this.speech.addText(`Starting with the workout ${workout.key}. `
-                + `Get ready. The first exercise will be ${this.user().data.workout.exercises[this.user().data.exercise++ % this.user().data.workout.exercises.length].exercise}.`)
-                .addAudio(countdownSound));
-    },
-
-    "NextExerciseIntent": function(exercise) {
-        if(!(this.user().data.workout)) {
-            this.toIntent("LAUNCH");
-        }
-        let nextExercise = this.user().data.workout.exercises[this.user().data.exercise++ % this.user().data.workout.exercises.length];
-        let speech = "";
-
-        if(this.user().data.exercise % this.user().data.workout.exercises.length === 0) {
-            this.user().data.rounds++;
-        }
-        if(this.user().data.rounds >= this.user().data.workout.rounds) {
-            this.toStateIntent("FinishWorkoutState", "FinishWorkoutIntent");
-            return;
-        }
-
-        if(nextExercise.rest) {
-            speech = `Rest for ${nextExercise.rest}.`;
-            this.alexaSkill().audioPlayer()
-                .play(restSound, "token")
-                .tell(speech);
-        } else if(nextExercise.exercise) {
-            speech = `Coming to the next exercise: ${nextExercise.exercise}.`;
-            this.tell(speech);
-        }
-    },
-
-    "CancelWorkoutIntent": function(workout) {
-        let speech = "Are you sure to cancel the workout? There are no excuses.";
-        let reprompt = "Do you want to cancel the workout?";
-        this.followUpState("CancelWorkoutState").ask(speech, reprompt);
-    },
-
-    "CancelWorkoutState": {
-        "YesIntent": function() {
-            this.user().data.exercise = 0;
-            this.user().data.rounds = 0;
-            this.alexaSkill().audioPlayer().stop().tell("You cancelled your current workout. I hope you feel bad.");
-        },
-
-        "NoIntent": function() {
-            this.tell("Continuing with your workout.");
-        },
-
-        "Unhandled": function() {
-            this.followUpState("CancelWorkoutState").ask("Do you want to cancel your current workout?", "");
-        }
-    },
-
-    "FinishWorkoutIntent": function(workout) {
-        if(this.user().data.rounds !== this.user().data.workout.rounds) {
-            this.tell(`You haven't done ${this.user().data.workout.rounds} rounds yet.`);
-        } else {
-            let speech = "Are you sure to finish your workout?";
-            let reprompt = "Do you want to finish your workout?";
-            this.followUpState("FinishWorkoutState").ask(speech, reprompt);
-        }
-    },
-
-    "FinishWorkoutState": {
-        "FinishWorkoutIntent": function() {
-            let workout = this.user().data.workout.title;
-            this.user().data.exercise = 0;
-            this.user().data.rounds = 0;
-            this.user().data.workout = {};
-            this.alexaSkill().audioPlayer().stop().tell(`You successfully finished ${workout}.`);
-        },
-
-        "YesIntent": function() {
-            this.user().data.exercise = 0;
-            this.user().data.rounds = 0;
-            this.user().data.workout = {};
-            this.alexaSkill().audioPlayer().stop().tell("Great job!");
-        },
-
-        "NoIntent": function() {
-            this.tell("Continuing with your workout.");
-        },
-
-        "Unhandled": function() {
-            this.followUpState("FinishWorkoutState").ask("Do you want to finish your current workout?", "");
-        }
+    "Unhandled": function () {
+        this.tell('Unhandled');
     },
 
 
     'AUDIOPLAYER': {
-        'AudioPlayer.PlaybackStarted': function() {
-            this.user().data.key = 'value';
-            console.log('AudioPlayer.PlaybackStarted');
-            this.endSession();
+        'AudioPlayer.PlaybackStarted': function () {
+            this.tell('Playback Started.')
         },
 
-        'AudioPlayer.PlaybackNearlyFinished': function() {
+        'AudioPlayer.PlaybackNearlyFinished': function () {
             console.log('AudioPlayer.PlaybackNearlyFinished');
             this.endSession();
         },
 
-        'AudioPlayer.PlaybackFinished': function() {
+        'AudioPlayer.PlaybackFinished': function () {
             console.log('AudioPlayer.PlaybackFinished');
             this.endSession();
         },
 
-        'AudioPlayer.PlaybackStopped': function() {
+        'AudioPlayer.PlaybackStopped': function () {
             console.log('AudioPlayer.PlaybackStopped');
             this.endSession();
         },
--- test/RoutingTest.js
+++ test/RoutingTest.js
@@ -5,82 +5,154 @@
  * @author Ruben Aegerter
  */
 
+// Configuration
+const {app} = require('../app/app');
 const expect = require('chai').expect;
-const {spawn} = require('child_process');
-const {alexaRequestBuilder, googleActionRequestBuilder, send, addUserData, getUserData, removeUser, removeUserData} = require('jovo-framework').Jester;
+const {alexaRequestBuilder, googleActionRequestBuilder, send, addUserData, addUser, removeUserData, removeUser, getUserData, removeSessionAttributes, setDbPath} = require('jovo-framework').Jester;
+setDbPath(app.config.db.localDbFilename);
 
-describe('LAUNCH', function () {
-    for (let req of [alexaRequestBuilder, googleActionRequestBuilder]) {
-        it('should jump into LaunchIntent for ' + req.type(), function (done) {
-            send(req.launch())
-                .then((res) => {
-                    expect(res.isAsk(['Hey?', 'Hello?', 'Good Morning?'], 'Hello World?')).to.equal(true);
-                    done();
+describe('REQUEST', function () {
+    describe('LAUNCH', function () {
+        for (let requestBuilder of [alexaRequestBuilder, googleActionRequestBuilder]) {
+            it('should jump into LaunchIntent for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.launch())
+                    .then((res) => {
+                        expect(res.isAsk(['Hey?', 'Hello?', 'Good Morning?'], 'Hello World?')).to.equal(true);
+                        done();
+                    })
+            })
+        }
+    });
+
+    describe('INTENT', function () {
+        for (let requestBuilder of [alexaRequestBuilder, googleActionRequestBuilder]) {
+            it('should send a default intent for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent())
+                    .then((res) => {
+                        expect(res.isTell('I\'m happy to help you.')).to.equal(true);
+                        done();
+                    })
+            });
+            it('should successfully change the intent name and add slots by passing them as parameters for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent('TestForSlotIntent', {name: 'John'}))
+                    .then((res) => {
+                        expect(res.isTell('Hey John!')).to.equal(true);
+                        done();
+                    });
+            });
+            it('should successfully change the intent name and add slots by calling the respective functions for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent().setIntentName('TestForSlotIntent').addInput('name', 'John'))
+                    .then((res) => {
+                        expect(res.isTell('Hey John!')).to.equal(true);
+                        done();
+                    })
+            });
+            it('should set session attribute for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent('TestSessionAttributesIntent').setSessionAttribute('STATE', 'TestSessionAttributesState'))
+                    .then((res) => {
+                        expect(res.isTell('I\'m in a state.')).to.equal(true);
+                        done();
+                    });
+            });
+            it('should set state for ' + googleActionRequestBuilder.type(), function (done) {
+                send(googleActionRequestBuilder.intent('TestSessionAttributesIntent').setState('TestSessionAttributesState'))
+                    .then((res) => {
+                        expect(res.isTell('I\'m in a state.')).to.equal(true);
+                        done();
+                    });
+            });
+            it('should check, if session is new for ' + googleActionRequestBuilder.type(), function (done) {
+                send(googleActionRequestBuilder.intent().setSessionNew())
+                    .then((res) => {
+                        expect(getUserData(requestBuilder.intent().getUserId(), 'new')).to.equal(true);
+                        done();
+                    });
+            });
+            it('should set user id for ' + requestBuilder.type(), function () {
+                let req = requestBuilder.intent().setUserId('1234');
+                expect(req.getUserId()).to.equal('1234');
+            });
+            it('should take an own request object for ' + requestBuilder.type(), function (done) {
+                send(require('./helper/record_1509465080969/google-action/1_req_HelpIntent'))
+                    .then((res) => {
+                        expect(res.isTell('I\'m happy to help you.')).to.equal(true);
+                        done();
+                    })
+            });
+        }
+    });
+
+    describe('END', function () {
+        for (let requestBuilder of [alexaRequestBuilder, googleActionRequestBuilder]) {
+            it('should send an EndRequest for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.end())
+                    .then((res) => {
+                        expect(res.isTell('Session ended.')).to.equal(true);
+                        done();
+                    })
+            })
+        }
+    });
+
+    describe('ALEXA SPECIFICS', function () {
+        for (let requestBuilder of [alexaRequestBuilder]) {
+            describe('AUDIO', function () {
+                it('should successfully send an AudioPlayer directive for ' + requestBuilder.type(), function (done) {
+                    send(requestBuilder.audio())
+                        .then((res) => {
+                            expect(res.getShouldEndSession()).to.equal(true);
+                            done();
+                        });
+                });
+                it('should successfully change the default directive for ' + requestBuilder.type(), function (done) {
+                    send(requestBuilder.audio('PlaybackStopped'))
+                        .then((res) => {
+                            expect(res.getShouldEndSession()).to.equal(true);
+                            send(requestBuilder.audio().setType('AudioPlayer.PlaybackStopped'))
+                                .then((res) => {
+                                    expect(res.getShouldEndSession()).to.equal(true);
+                                    done();
+                                });
+                        })
+                });
+                it('should send a StopIntent for ' + requestBuilder.type(), function (done) {
+                    send(requestBuilder.intent('AMAZON.StopIntent'))
+                        .then((res) => {
+                            expect(res.isTell('Audio stopped.')).to.equal(true);
+                            done();
+                        })
                 })
-        })
-    }
-
-});
-
-describe.only('START WORKOUT FOR ALEXA', function () {
-    for (let req of [googleActionRequestBuilder]) {
-        it('should start a new workout and save the data in a db.json file', function (done) {
-            this.timeout(5000);
-            send(req.launch())
-                .then(() => send(req.intent('TestIntent')))
-                .then(() => send(req.intent('YesIntent')))
-                .then((res) => {
-                    expect(res.isTell('YES')).to.equal(true);
-                    done();
+            });
+            describe.only('ERROR', function () {
+                it('should send an error for ' + requestBuilder.type(), function(done) {
+                    send(requestBuilder.error())
+                        .then((res) => {
+                            done();
+                        })
                 })
-        });
-    }
-});
-
-describe('FINISH WORKOUT', function () {
-    it('should finish the workout after 4 rounds', function (done) {
-        this.timeout(10000);
-        send(googleActionRequestBuilder.intent('TestIntent'))
-            .then(send(googleActionRequestBuilder.intent('YesIntent')))
-            .then((res) => {
-                expect(res.isTell('YES')).to.equal(true);
-                done();
-            })
-            .catch((err) => {
-                done(err);
-            })
-    })
-});
-
-describe('HelpIntent', function () {
-    // TODO if this fails then user has no HelpIntent defined, which is essential for certification
-    it('should successfully go into HelpIntent', function (done) {
-        this.timeout(5000);
-        send(alexaRequestBuilder.intent('HelpIntent'))
-            .then((res) => {
-                expect(res.isTell('I\'m happy to help.')).to.equal(true);
-                done();
-            })
-            .catch((err) => {
-                if (err.code === 'ESOCKETTIMEDOUT') {
-                    done(new Error('You have to define a HelpIntent'));
-                }
-            })
+            });
+        }
     });
 });
 
-describe('Unhandled', function () {
-    it('should go into Unhandled when requestIntent is defined as Unhandled', function (done) {
-        this.timeout(5000);
-        send(alexaRequestBuilder.intent('Unhandled'))
-            .then((res) => {
-                expect(res.isTell('I can\'t help you with that.')).to.equal(true);
-                done();
-            })
-            .catch((err) => {
-                if (err.code === 'ESOCKETTIMEDOUT') {
-                    done(new Error('You have to define a UnhandledIntent'));
-                }
-            })
+describe('RESPONSE', function () {
+    describe('Session Attributes', function () {
+        for (let requestBuilder of [alexaRequestBuilder, googleActionRequestBuilder]) {
+            it('should include session attribute in response for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent('TestSessionAttributesIntent'))
+                    .then((res) => {
+                        expect(res.hasSessionAttribute('STATE', 'TestSessionAttributesState')).to.equal(true);
+                        done();
+                    });
+            });
+            it('should automatically set session attributes for the next intent for ' + requestBuilder.type(), function (done) {
+                send(requestBuilder.intent('TestSessionAttributesIntent'))
+                    .then(() => send(requestBuilder.intent('TestUnhandledIntent')))
+                    .then((res) => {
+                        expect(res.isTell('Unhandled in a state.')).to.equal(true);
+                        done();
+                    })
+            });
+        }
     });
 });
