4d628b70f84410cdab8c71089fc786e9a1d3a9dd
Ruben Aegerter <johndoe@example.com>
2018-05-22 13:02:21 +0200

    add new tests

--- test/RoutingTest.js
+++ test/RoutingTest.js
@@ -14,9 +14,9 @@ setDbPath(app.config.db.localDbFilename);
 
 describe('REQUEST', function () {
     describe('LAUNCH', function () {
-        for (let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-            it('should jump into LaunchIntent for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.launch())
+        for (let rb of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
+            it('should jump into LaunchIntent for ' + rb.type(), function (done) {
+                send(rb.launch())
                     .then((res) => {
                         expect(res.isAsk(['Hey?', 'Hello?', 'Good Morning?'], 'Hello World?')).to.equal(true);
                         done();
@@ -26,71 +26,67 @@ describe('REQUEST', function () {
     });
 
     describe('INTENT', function () {
-        for (let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-            it('should send a default intent for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent())
+        for (let rb of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
+            it('should send a default intent for ' + rb.type(), function (done) {
+                send(rb.intent())
                     .then((res) => {
                         expect(res.isTell('I\'m happy to help you.')).to.equal(true);
                         done();
                     })
             });
-            it('should save the launch intent in a variable for later usage for ' + requestBuilder.type(), function(done) {
-                let intent = requestBuilder.intent();
+            it('should save the launch intent in a variable for later usage for ' + rb.type(), function (done) {
+                let intent = rb.intent();
                 send(intent)
                     .then((res) => {
                         expect(res.isTell('I\'m happy to help you.')).to.equal(true);
                         done();
                     })
             });
-            it('should successfully change the intent name and add slots by passing them as parameters for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent('TestForSlotIntent', {name: 'John'}))
+            it('should successfully change the intent name and add slots by passing them as parameters for ' + rb.type(), function (done) {
+                send(rb.intent('TestForSlotIntent', {name: 'John'}))
                     .then((res) => {
                         expect(res.isTell('Hey John!')).to.equal(true);
                         done();
                     });
 
             });
-            it('should successfully change the intent name and add slots by calling the respective functions for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent().setIntentName('TestForSlotIntent').addInput('name', 'John'))
+            it('should successfully change the intent name and add slots by calling the respective functions for ' + rb.type(), function (done) {
+                send(rb.intent().setIntentName('TestForSlotIntent').addInput('name', 'John'))
                     .then((res) => {
                         expect(res.isTell('Hey John!')).to.equal(true);
                         done();
                     })
             });
-            it('should set session attribute for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent('TestSessionAttributesIntent').setSessionAttribute('STATE', 'TestSessionAttributesState'))
+            it('should set session attribute for ' + rb.type(), function (done) {
+                send(rb.intent('TestSessionAttributesIntent').setSessionAttribute('STATE', 'TestSessionAttributesState'))
                     .then((res) => {
                         expect(res.isTell('I\'m in a state.')).to.equal(true);
                         done();
                     });
             });
-            it('should delete all session attributes, if the session is new for ' + requestBuilder.type(), function(done) {
-                send(requestBuilder.intent('TestSessionAttributesIntent').setState('TestSessionAttributesState'))
-                    .then(() => send(requestBuilder.launch()))
+            it('should delete all session attributes, if the session is new for ' + rb.type(), function (done) {
+                send(rb.intent('TestSessionAttributesIntent').setState('TestSessionAttributesState'))
+                    .then(() => send(rb.launch()))
                     .then((res) => {
                         expect(res.hasSessionAttributes()).to.equal(false);
                         done();
                     })
             });
-            it('should set state for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent('TestSessionAttributesIntent').setState('TestSessionAttributesState'))
+            it('should set state for ' + rb.type(), function (done) {
+                send(rb.intent('TestSessionAttributesIntent').setState('TestSessionAttributesState'))
                     .then((res) => {
                         expect(res.isTell('I\'m in a state.')).to.equal(true);
                         done();
                     });
             });
-            it('should check, if session is new for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent().setSessionNew())
+            it('should check, if session is new for ' + rb.type(), function (done) {
+                send(rb.intent().setSessionNew())
                     .then((res) => {
-                        expect(getUserData(requestBuilder.intent().getUserId(), 'new')).to.equal(true);
+                        expect(getUserData(rb.intent().getUserId(), 'new')).to.equal(true);
                         done();
                     });
             });
-            it('should set user id for ' + requestBuilder.type(), function () {
-                let req = requestBuilder.intent().setUserId('1234');
-                expect(req.getUserId()).to.equal('1234');
-            });
-            it('should take an own request object for ' + requestBuilder.type(), function (done) {
+            it('should take an own request object for ' + rb.type(), function (done) {
                 send(require('./recordings/Hello/AlexaSkill/01_req_LAUNCH'))
                     .then((res) => {
                         expect(res.isAsk(['Hey?', 'Hello?', 'Good Morning?'], 'Hello World?')).to.equal(true);
@@ -101,9 +97,9 @@ describe('REQUEST', function () {
     });
 
     describe('END', function () {
-        for (let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-            it('should send an EndRequest for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.end())
+        for (let rb of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
+            it('should send an EndRequest for ' + rb.type(), function (done) {
+                send(rb.end())
                     .then((res) => {
                         expect(res.isTell('Session ended.')).to.equal(true);
                         done();
@@ -113,28 +109,28 @@ describe('REQUEST', function () {
     });
 
     describe('ALEXA SPECIFICS', function () {
-        for (let requestBuilder of getPlatformRequestBuilder('AlexaSkill')) {
+        for (let rb of getPlatformRequestBuilder('AlexaSkill')) {
             describe('AUDIO', function () {
-                it('should successfully send an AudioPlayer directive for ' + requestBuilder.type(), function (done) {
-                    send(requestBuilder.audioPlayerRequest())
+                it('should successfully send an AudioPlayer directive for ' + rb.type(), function (done) {
+                    send(rb.audioPlayerRequest())
                         .then((res) => {
                             expect(res.getShouldEndSession()).to.equal(true);
                             done();
                         });
                 });
-                it.skip('should successfully change the default directive for ' + requestBuilder.type(), function (done) {
-                    send(requestBuilder.audioPlayerRequest('PlaybackStopped'))
+                it.skip('should successfully change the default directive for ' + rb.type(), function (done) {
+                    send(rb.audioPlayerRequest('PlaybackStopped'))
                         .then((res) => {
                             expect(res.getShouldEndSession()).to.equal(true);
-                            send(requestBuilder.audioPlayerRequest().setType('AudioPlayer.PlaybackStopped'))
+                            send(rb.audioPlayerRequest().setType('AudioPlayer.PlaybackStopped'))
                                 .then((res) => {
                                     expect(res.getShouldEndSession()).to.equal(true);
                                     done();
                                 });
                         })
                 });
-                it('should send a StopIntent for ' + requestBuilder.type(), function (done) {
-                    send(requestBuilder.intent('AMAZON.StopIntent'))
+                it('should send a StopIntent for ' + rb.type(), function (done) {
+                    send(rb.intent('AMAZON.StopIntent'))
                         .then((res) => {
                             expect(res.isTell('Audio stopped.')).to.equal(true);
                             done();
@@ -142,26 +138,26 @@ describe('REQUEST', function () {
                 })
             });
             describe('ERROR', function () {
-                it('should send an error for ' + requestBuilder.type(), function(done) {
-                    send(requestBuilder.errorRequest())
+                it('should send an error for ' + rb.type(), function (done) {
+                    send(rb.errorRequest())
                         .then((res) => {
                             expect(res.isTell('Session ended.')).to.equal(true);
                             done();
                         })
                 })
             });
-            describe('SKILL EVENT REQUEST', function() {
-                it('should just send a default skill event request for ' + requestBuilder.type(), function(done) {
-                    send(requestBuilder.skillEventRequest())
+            describe('SKILL EVENT REQUEST', function () {
+                it('should just send a default skill event request for ' + rb.type(), function (done) {
+                    send(rb.skillEventRequest())
                         .then((res) => {
                             //expect(res.isTell('Skill Event fired!')).to.equal(true);
                             done();
                         })
                 })
             });
-            describe('DISPLAY REQUEST', function() {
-                it('should just send a default display request for ' + requestBuilder.type(), function(done) {
-                    send(requestBuilder.displayRequest())
+            describe('DISPLAY REQUEST', function () {
+                it('should just send a default display request for ' + rb.type(), function (done) {
+                    send(rb.displayRequest())
                         .then((res) => {
                             done();
                         })
@@ -171,34 +167,50 @@ describe('REQUEST', function () {
     });
 });
 
-describe('USER DATA', function() {
-    describe('ADD USER DATA', function() {
-        it('should add user data and get it successfully', function() {
+describe('USER DATA', function () {
+    describe('ADD USER DATA', function () {
+        it('should add user data and get it successfully', function () {
             addUserData('1234', 'key', 'value');
             expect(getUserData('1234', 'key') === 'value').to.equal(true);
             removeUser();
         });
-        it('should add multiple users without altering data for other users', function() {
+        it('should add multiple users without altering data for other users', function () {
             addUserData('1234', 'key', 'value');
             addUserData('123');
             expect(getUserData('123', 'hello') === undefined).to.equal(true);
         })
+    });
+
+    describe('USER_ID', function () {
+        for (let rb of getPlatformRequestBuilder('AlexaSkill', 'GoogleActionDialogFlow')) {
+            it('should set user id for ' + rb.type(), function () {
+                let req = rb.intent().setUserId('1234');
+                expect(req.getUserId()).to.equal('1234');
+            });
+            it('should send an intent with a set user id for ' + rb.type(), function(done) {
+                let req = rb.intent().setUserId('1234');
+                send(req)
+                    .then((res) => {
+                        done();
+                    });
+            });
+        }
     })
 });
 
 describe('RESPONSE', function () {
     describe('Session Attributes', function () {
-        for (let requestBuilder of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
-            it('should include session attribute in response for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent('TestSessionAttributesIntent'))
+        for (let rb of getPlatformRequestBuilder('GoogleActionDialogFlow', 'AlexaSkill')) {
+            it('should include session attribute in response for ' + rb.type(), function (done) {
+                send(rb.intent('TestSessionAttributesIntent'))
                     .then((res) => {
                         expect(res.hasSessionAttribute('STATE', 'TestSessionAttributesState')).to.equal(true);
                         done();
                     });
             });
-            it('should automatically set session attributes for the next intent for ' + requestBuilder.type(), function (done) {
-                send(requestBuilder.intent('TestSessionAttributesIntent'))
-                    .then(() => send(requestBuilder.intent('TestUnhandledIntent')))
+            it('should automatically set session attributes for the next intent for ' + rb.type(), function (done) {
+                send(rb.intent('TestSessionAttributesIntent'))
+                    .then(() => send(rb.intent('TestUnhandledIntent')))
                     .then((res) => {
                         expect(res.isTell('Unhandled in a state.')).to.equal(true);
                         done();
